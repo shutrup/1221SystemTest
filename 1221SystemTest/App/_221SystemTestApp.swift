4340b82bef5c7df04eed5b59f12b04dc7393f019
shutrup <magagaraev95@mail.ru>
2024-01-30 17:22:15 +0300

    setup MainTabBar

--- 1221SystemTest/App/_221SystemTestApp.swift
+++ 1221SystemTest/App/_221SystemTestApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct _221SystemTestApp: App {
     var body: some Scene {
         WindowGroup {
-            DetailView()
+            MainTabBar()
         }
     }
 }
---
+++ 1221SystemTest/Views/Main TabBar/MainTabBar.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct MainTabBar: View {
+    var body: some View {
+        TabView {
+            DetailView()
+                .tabItem { Label("Главная", systemImage: "tree.circle.fill") }
+            
+            Text("Каталог")
+                .tabItem { Label("Каталог", systemImage: "circle.grid.2x2") }
+            
+            Text("Корзина")
+                .tabItem { Label("Корзина", systemImage: "cart.fill") }
+            
+            Text("Профиль")
+                .tabItem { Label("Профиль", systemImage: "person") }
+        }
+        .accentColor(.green)
+    }
+}
+
+#Preview {
+    MainTabBar()
+}
