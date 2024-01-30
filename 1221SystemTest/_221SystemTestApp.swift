3bb603da56596dc72ffc7de2aacfeb3291fa5516
shutrup <magagaraev95@mail.ru>
2024-01-30 17:09:09 +0300

    configure project

--- 1221SystemTest/_221SystemTestApp.swift
+++ 1221SystemTest/_221SystemTestApp.swift
@@ -1,17 +1,10 @@
-//
-//  _221SystemTestApp.swift
-//  1221SystemTest
-//
-//  Created by Шарап Бамматов on 30.01.2024.
-//
-
 import SwiftUI
 
 @main
 struct _221SystemTestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DetailView()
         }
     }
 }
